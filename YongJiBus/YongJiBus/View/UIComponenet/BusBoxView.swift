abca9839af734c38ae35553e22197c5e13f6e9ec
dogyungkim <prunsoli12@gmail.com>
2024-09-13 16:20:55 +0900

    feat:버스 노선 번호에 따라 BusBoxView 넓이 조절

--- YongJiBus/YongJiBus/View/UIComponenet/BusBoxView.swift
+++ YongJiBus/YongJiBus/View/UIComponenet/BusBoxView.swift
@@ -10,69 +10,92 @@ import SwiftUI
 struct BusBoxView: View {
     
     @ObservedObject var viewModel = BusBoxViewViewModel()
+    @State private var isLoading = false
     
-    private var title : String
-    private let busNumber : BusNumber
+    private var title: String
+    private let busNumber: BusNumber
     
-    init(_ busNumber : BusNumber){
+    init(_ busNumber: BusNumber) {
         self.busNumber = busNumber
         self.title = busNumber.title
     }
 
     var body: some View {
         Button(action: {
-            viewModel.loading()
-            viewModel.load(busNumber){
-                viewModel.stopLoading()
+            isLoading = true
+            viewModel.load(busNumber) {
+                isLoading = false
             }
         }, label: {
-            HStack{
-                ZStack{
-                    Circle()
-                        .fill(.white)
-                        .frame(width: 50,height: 55)
-                    if viewModel.isLoading {
-                        ProgressView()
-                            .progressViewStyle(.circular)
-                            .scaleEffect(1.5, anchor: .center)
-                            .tint(Color("BusColor"))
-                            .frame(width: 50,height: 50)
-                    } else {
-                        Image(systemName: "bus")
-                            .resizable()
-                            .frame(width: 30,height: 30)
-                            .foregroundStyle(Color("BusColor"))
-                    }
-                }
-                VStack(alignment: .leading){
-                    Text(title)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.black)
-                    ZStack{
-                        Text(viewModel.text)
-                            .font(.body)
-                            .foregroundStyle(.black)
-                    }
-                }
+            HStack {
+                BusIconView(isLoading: isLoading)
+                    .padding(.leading, 10)
+                BusInfoView(title: title, text: viewModel.text)
+                    .padding(.trailing, 10)
             }
-            .frame(width: 165,height: 85)
+            .frame(height: 85)
             .background(Color("BusColor"))
             .clipShape(RoundedRectangle(cornerRadius: 10))
         })
         .buttonStyle(.plain)
-        .onAppear{
-            viewModel.loading()
-            viewModel.load(busNumber){
-                viewModel.stopLoading()
+        .onAppear {
+            isLoading = true
+            viewModel.load(busNumber) {
+                isLoading = false
+            }
+        }
+    }
+}
+
+// LoadingIndicatorView.swift
+struct BusIconView: View {
+    var isLoading: Bool
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.white)
+                .frame(width: 50, height: 55)
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .scaleEffect(1.5, anchor: .center)
+                    .tint(Color("BusColor"))
+                    .frame(width: 50, height: 50)
+            } else {
+                Image(systemName: "bus")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundStyle(Color("BusColor"))
+            }
+        }
+    }
+}
+
+// BusInfoView.swift
+struct BusInfoView: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundStyle(.black)
+            ZStack {
+                Text(text)
+                    .font(.body)
+                    .foregroundStyle(.black)
             }
         }
     }
 }
 
 
+
 struct BusBoxView_Previews: PreviewProvider {
     static var previews: some View {
-        BusBoxView(.oneA)
+        BusBoxView(.zero)
     }
 }
